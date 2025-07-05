3244320e70ea93b5861289b215ca1d8fd61134ff
direncakbay <bilgi@akbayhukuk.com>
2025-07-05 11:53:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 // Firebase Imports
 import { initializeApp } from "firebase/app";
